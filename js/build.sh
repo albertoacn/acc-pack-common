9d9f7e0744c1a017c3ec7c063a4260e0848aec69
acn <acn@MacBookCapitanUK-3.local>
2016-11-29 15:47:09 +0100

    Adding build scripts for iOS and JS

--- js/build.sh
+++ js/build.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 set -e
+<<<<<<< HEAD
+task="$1"
+reltype="$2"
+
+if [[ "$task" == "-np" && "$reltype" != "patch" && "$reltype" != "minor" && "$reltype" != "major" ]];
+then
+	echo Invalid second parameter.  Please specify 'patch', 'minor' or 'major'.
+	exit 1
+fi
+=======
+>>>>>>> upstream/sdk-wrapper
 
 if [[ -d src ]]
 then
 	gulp dist
 	gulp zip
+<<<<<<< HEAD
+=======
   cd dist
 
+>>>>>>> upstream/sdk-wrapper
 else
 	echo "Please run this script from 'JS'."
 	exit 1
 fi
+<<<<<<< HEAD
+
+if [[ $task == "-np" ]]
+then
+	npm version $reltype
+	git add .
+	git commit -m "New build. Bump npm version."
+	npm publish
+fi
+=======
+>>>>>>> upstream/sdk-wrapper
